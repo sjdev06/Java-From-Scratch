421e112ad7ea2a9bdc1eff6b5687003499cbb7d3
Saket Jha <saketj.dev@gmail.com>
2025-12-22 20:12:41 +0530

    datatypes of all kind

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+public class datatypes {
+    public static void main(String a[])
+    {
+        int num1 = 9;
+        byte by = 123;
+        short sh = 124;
+        long l = 5845l;
+        float f = 12.2f;
+        double d = 12.23;
+        char c = 'k';
+        boolean b = true;
+
+
+    }
+    
+}
