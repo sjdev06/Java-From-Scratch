2a9356c98f8b987e1910197380f1ba93cccbb735
Saket Jha <saketj.dev@gmail.com>
2026-01-03 20:29:55 +0530

    can have multiple system.out in a single main block

--- TC.java
+++ TC.java
@@ -3,6 +3,11 @@ public class TC {
         int b = 12;
         byte k = (byte)b;
         System.out.println(k);
-    }
     
+        int num1 = 7;
+        num1 = num1 + 4;
+        System.out.println(num1);
 }
+}
+
+    
